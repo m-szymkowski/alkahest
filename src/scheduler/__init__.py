7ee668f5aa28f920738566a3e7ae916ba434adcf
m-szymkowski <marcin.szymkowski2@poczta.onet.pl>
2026-09-13 21:15:29 +0200

    Project structure

---
+++ src/scheduler/__init__.py
@@ -0,0 +1,11 @@
+"""
+Design a task scheduler supporting one-time and recurring jobs, cancellation, concurrent execution, and retries.
+"""
+
+
+def main() -> None:
+    print("Tasks!")
+
+
+if __name__ == "__main__":
+    main()
